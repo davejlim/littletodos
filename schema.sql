848aa6031538ae01c948ba604dce4682bc0433d0
davejlim <7742500+davejlim@users.noreply.github.com>
2023-12-18 22:57:28 +0000

    Production ready

--- schema.sql
+++ schema.sql
@@ -7,5 +7,5 @@ CREATE TABLE todos (
   id serial PRIMARY KEY,
   name text NOT NULL,
   completed boolean NOT NULL DEFAULT 'false',
-  list_id integer NOT NULL  REFERENCES list(id) ON DELETE CASCADE
+  list_id integer NOT NULL  REFERENCES lists(id) ON DELETE CASCADE
 );
